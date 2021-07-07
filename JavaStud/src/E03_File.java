6ad4ab3b6aa315f871c7b333fcd206d76e43e0e0
K <K@DESKTOP-S1ACD8T>
2021-07-07 16:05:18 +0900

    중간과정

--- JavaStud/src/E03_File.java
+++ JavaStud/src/E03_File.java
@@ -10,6 +10,9 @@ public class E03_File {
 	public static void main(String[] args) {
 		File f1 = new File("note/Git 사용 설명서.txt");
 		File f2 = new File("note/Git 사용 설명서");
+		File f3 = new File("files");
+		File f4 = new File(f3,"game.sav");// parent, child
+		File f5 = new File("image");
 		
 		System.out.println("f1이 존재합니까? "+f1.exists());
 		System.out.println("f1이 존재합니까? "+f2.exists());
@@ -17,5 +20,21 @@ public class E03_File {
 		System.out.println("읽기 권한이 있는 파일인가요? "+f1.canRead());
 		System.out.println("쓰기 권한이 있는 파일인가요? "+f1.canWrite());
 		System.out.println("실행 권한이 있는 파일인가요? "+f1.canExecute());
+		
+		System.out.println("f1에 담겨있는 경로가 절대 경로인가요? "+f1.isAbsolute());
+		System.out.println("f1의 절대 경로 : "+f1.getAbsolutePath());
+		System.out.println("f1은 디렉토리인가요? "+f1.isDirectory());
+		
+		System.out.println(f1);
+		System.out.println(f3.isDirectory());
+		System.out.println(f4);
+		System.out.println(f4.getAbsolutePath());
+		
+		if(!f5.exists()) {
+			f5.mkdir(); // 해당 경로로 새로운 대렉토리(폴더) 생성
+			System.out.println("새로 생성함.");
+		}else {
+			System.out.println("이미 존재해서 생성하지 않음.");
+		}
 	}
 }
